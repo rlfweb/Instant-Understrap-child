cb20e133dd30eb1df564104a0f62bf2a518df388
REBECCA FRENCH <rlfweb8@gmail.com>
2022-05-02 15:20:41 +0100

    Custom page Template for e.g. a landing page

---
+++ page-templates/page-landing.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Template Name: Custom Landing Page
+ *
+ * Template for Landing Page
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+
+if ( is_front_page() ) {
+	get_template_part( 'global-templates/hero' );
+}
+?>
+
+<div class="wrapper bg-primary" id="full-width-page-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content">
+
+		<div class="row">
+
+			<div class="col-md-8 offset-md-2 content-area" id="primary">
+
+				<main class="site-main card" id="main" role="main">
+
+				<div class="card-body">
+					<?php
+					while ( have_posts() ) {
+						the_post();
+						get_template_part( 'loop-templates/content', 'page' );
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) {
+							comments_template();
+						}
+					}
+					?>
+				</div>
+
+				</main><!-- #main -->
+
+			</div><!-- #primary -->
+
+		</div><!-- .row end -->
+
+	</div><!-- #content -->
+
+</div><!-- #full-width-page-wrapper -->
+
+<?php
+get_footer();
